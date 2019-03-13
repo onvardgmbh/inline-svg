d2861b915a2fbd32a8f0912911fd8a0f15b457bf
Björn Hasse <hasse@onvard.de>
2019-03-13 11:37:21 +0100

    Add boxicons

--- InlineSVG.php
+++ InlineSVG.php
@@ -26,6 +26,12 @@ class InlineSVG {
 		return $object;
 	}
 
+	public static function boxicons( $name ) {
+		$object = new InlineSVG();
+		$object->svg = file_get_contents(__DIR__ .'/../../atisawd/boxicons/static/img/svg/' . $name . '.svg');
+		return $object;
+	}
+
 	public function width($width) {
 		$this->svg = preg_replace('/(width=")\d*(p?x?")/', '${1}' . $width . '${2}', $this->svg, 1);
 		return $this;
@@ -53,6 +59,19 @@ class InlineSVG {
 		    ->prepend('')
 		    ->toArray();
     }
+
+	public static function getOptionsBoxicons() {
+
+		return collect(scandir(__DIR__ .'/../../atisawd/boxicons/static/img/svg'))
+			->filter(function($item) {
+				return !collect(['.', '..'])->contains($item);
+			})
+			->mapWithKeys(function($item) {
+				return [ str_replace('.svg', '', $item) => str_replace('.svg', '', $item)];
+			})
+			->prepend('')
+			->toArray();
+	}
 
 	public function get() {
 		return $this->svg;
